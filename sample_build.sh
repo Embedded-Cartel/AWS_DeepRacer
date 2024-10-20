7e6fd36160e7c5075e35c6a53431654a9d3df9f0
yumdari <epqmfzkdlwj@naver.com>
2024-10-20 08:04:02 +0000

    [refs: #7][common] 스크립트 수정  - export.sh 삭제  - build.sh 분기    기존 : 각 프로젝트 빌드 경로로 들어가서 build.sh 실행    변경 : 각 프로젝트의 빌드 경로로 들어가지 않고 스크립트 실행으로           빌드하도록 함([프로젝트명]_build.sh)  - install_sdk.sh 수정    sdk문제 발생을 대비하여 기존 sdk 삭제하는 명령어 추가

--- sample_build.sh
+++ sample_build.sh
@@ -1,5 +1,5 @@
+cd /home/ubuntu/AWS_DeepRacer/AUTOSAR/EC2024_Sample/gen_ara/implementation/Machine 
 cmake -B build -D CMAKE_INSTALL_PREFIX=$PARA_SDK
 cmake --build build -j $(nproc)
 cmake --install build
 rm -rf build
-cd $PRAR_SDK/bin
