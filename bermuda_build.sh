f27eb7622b66f9ed472a02c0b1e0b565387dd597
yumdari <epqmfzkdlwj@naver.com>
2024-10-20 08:40:38 -0400

    [refs: #7][common] build 스크립트 경로 수정 (절대 -> 상대)

--- bermuda_build.sh
+++ bermuda_build.sh
@@ -1,4 +1,4 @@
-cd /home/ubuntu/AWS_DeepRacer/AUTOSAR/bermuda/gen_ara/implementation/Machine 
+cd AUTOSAR/bermuda/gen_ara/implementation/Machine 
 cmake -B build -D CMAKE_INSTALL_PREFIX=$PARA_SDK
 cmake --build build -j $(nproc)
 cmake --install build
