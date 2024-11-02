1087b62d2b0eeebb4b12ba183a68c74edb929096
yumdari <epqmfzkdlwj@naver.com>
2024-11-02 05:35:56 -0400

    [refs: #11][Common] update build script

--- build_sample.sh
+++ build_sample.sh
@@ -1,4 +1,24 @@
-cd AA_Sample_Code
+#!/bin/bash
+
+cd AA_Sample 
+
+if ifconfig | grep -q "172.31.16.137"; then
+  echo "EC2"
+
+  find ./EM ./CM -type f -name "*.json" -exec sed -i 's/192.168.20.27/172.31.16.137/g' {} \;
+  find ./EM ./CM -type f -name "*.json" -exec sed -i 's/wlo1/ens5/g' {} \;
+
+elif ifconfig | grep -q "192.168.0.10"; then
+  echo "DeepRacer"
+
+  find ./EM ./CM -type f -name "*.json" -exec sed -i 's/192.168.20.27/192.168.0.10/g' {} \;
+  find ./EM ./CM -type f -name "*.json" -exec sed -i 's/wlo1/mlan0/g' {} \;
+
+
+else
+  echo "IP not found"
+fi
+
 cmake -B build -D CMAKE_INSTALL_PREFIX=$PARA_SDK
 cmake --build build -j $(nproc)
 cmake --install build
