6db389f2e778ee5359627b1c3f91dbbfe067c07d
yong-ggori <gyong2542@gmail.com>
2024-11-14 23:30:53 -0500

    [refs: #18][common] PARA_SDK 환경 변수 관련 내용 수정

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 #export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-export PARA_SDK=/home/deepracer/para_deepracer_release
+export PARA_SDK=$(pwd)/sdk
 export PARA_CORE=$PARA_SDK
 export PARA_CONF=$PARA_SDK/etc
 export PARA_APPL=$PARA_SDK/opt
