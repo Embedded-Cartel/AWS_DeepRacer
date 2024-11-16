e57949c8106eefcb6a292dc280e30fa0a3e50829
yumdari <epqmfzkdlwj@naver.com>
2024-11-16 14:31:22 +0900

    [refs: #35][common] 미사용 디렉토리 삭제

--- deepracercode/camera/build_camera.sh
+++
@@ -1,1 +0,0 @@
-g++ camera_node_test.cpp -o camera_node_test -I/usr/include/opencv4/ -L/usr/lib -lopencv_core -lopencv_imgproc -lopencv_videoio -lopencv_imgcodecs
--- deepracercode/servo/build_servo.sh
+++
@@ -1,1 +0,0 @@
-g++ -o servo_node_test bios_version.cpp led_mgr.cpp pwm.cpp servo_mgr.cpp servo_node_test.cpp utility.cpp jsoncpp.cpp -lstdc++fs -std=c++17
